49e3bbc2f665dc151cbaf4ce478bb3ddc55b281c
mass2010chromium <jcpeng2@illinois.edu>
2019-02-01 22:15:43 -0800

    Continued implementing game logic and tests

--- main/game_logic.py
+++ main/game_logic.py
@@ -1,21 +1,57 @@
 import random
+import numpy as np
 
-chance_of_4 = 1/12
+default_chance_of_4 = 1/15
 
 class Game(object):
-    
-    def __init__(self, seed):
+
+    def __init__(self, seed, board_size, four_chance = default_chance_of_4):
         random.seed(seed) # TODO: Initialize and store random seeds in a better way
-        self.board = [[0,0,0,0],
-                          [0,0,0,0],
-                          [0,0,0,0],
-                          [0,0,0,0]]
+        self.board_size = board_size
+        self.four_chance = four_chance
+        self.board = [([0] * self.board_size) for i in range(self.board_size)]
         for i in range(0, 2):
             self.spawn_tile()
     
 
     def get_board(self):
         return self.board
+
+    def clear_board(self):
+        """
+        Clears the board. (Zeros all the elements.)
+        """
+        self.board = [([0 * self.board_size]) for i in range(self.board_size)]
     
     def spawn_tile(self):
-        pass
+        """
+        Spawns a single tile in this game. Usually 2s, with a chance of 4.
+        Picks an empty tile to spawn a new tile in.
+        Returns true on success and false on failure.
+        """
+
+        # Find all of the empty tiles
+        empty_squares = [] # List of (row, col) coordinates that are empty in the grid.
+        for x, y in np.ndindex((self.board_size, self.board_size)):
+            tile = self.board[x][y]
+            if tile == 0:
+              empty_squares.append([x, y])
+
+        chosen_square = empty_squares[random.randint(0, len(empty_squares) - 1)]
+        print(empty_squares)
+        print(chosen_square)
+        next_num = 2
+        if random.random() > self.four_chance:
+          next_num = 4
+        self.get_board()[chosen_square[0]][chosen_square[1]] = next_num
+        if len(empty_squares) == 1 and not has_valid_move(self.get_board()):
+            return False
+        return True;
+
+    def has_valid_move(board):
+        """
+        Helper function to test if the given board has a valid move.
+        """
+        for row in len(board):
+            for col in len(board):
+              pass
--- main/test/test_game_logic.py
+++ main/test/test_game_logic.py
@@ -1,5 +1,57 @@
 import sys
 sys.path.append('../')
-from main import game_logic
 
-print(sys.path)
+from game_logic import Game
+import functools
+
+empty_board = [[0, 0, 0, 0],
+			   [0, 0, 0, 0],
+			   [0, 0, 0, 0],
+			   [0, 0, 0, 0]]
+
+test_board = [[0, 0, 2, 2],
+			  [0, 2, 0, 0],
+			  [0, 0, 2, 0],
+			  [2, 0, 2, 2]]
+
+def sum_tiles(board):
+	return functools.reduce(lambda x, y: x + sum(y), board, 0)
+
+def test_get_board(game):
+	print("Running test_get_board...")
+	board = game.get_board()
+	assert board, "failed get_board(): null board"
+	game.board = test_board
+	assert game.get_board() == test_board, "failed get_board(): Wrong board"
+	game.board = board
+	print("Passed test_get_board!")
+
+
+def test_clear_board(game):
+	print("Running test_clear_board...")
+	game.board = test_board
+	game.clear_board()
+	count = sum_tiles(game.get_board())
+	assert count == 0, "failed clear_board(): Board not empty"
+	print("Passed test_clear_board!")
+
+
+
+def test_spawn_tile(game):
+	print("Running test_spawn_tile...")
+	game.board = test_board;
+	tiles_pre = sum_tiles(game.get_board())
+	game.spawn_tile()
+	new_board = game.get_board()
+	tiles_post = sum_tiles(game.get_board())
+	assert (tiles_post == tiles_pre + 2) or (tiles_post == tiles_pre + 4), "failed spawn_tile(): Did not increase total tile sum"
+	print("Passed test_spawn_tile!")
+
+##  
+# Run the tests
+##
+test_game = Game(0, 4)
+
+test_get_board(test_game)
+test_clear_board(test_game)
+test_spawn_tile(test_game)
