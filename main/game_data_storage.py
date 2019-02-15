0192448888d937736e6fbf05d9388f2d3f2236ce
mass2010chromium <jcpeng2@illinois.edu>
2019-02-14 21:11:11 -0800

    Added tests for game_data_storage, half implementation of data_storage

--- main/game_data_storage.py
+++ main/game_data_storage.py
@@ -6,23 +6,42 @@ class DataStorage(object):
     """
     Dataframe structure:
 
-    First column: Board states
-    Columns 2-5: Moves (up, down, left, right)
+    Indices: Board states
+    Columns 1-4: Moves (up, down, left, right)
     """
 
     def __init__(self):
         self.game_list = [] # List of dataframes
         self.game_scores = [] # List of scores
 
-    """
-    Add a game's worth of data to this storage.
 
-    board_states: list of board states.
-    move: list of moves, 1:1 with board states.
-    final_score: final score.
-    """
     def add_game_data(self, board_states, moves, final_score):
-        # TODO: get moves properly
+        """
+        Add a game's worth of data to this storage.
+
+        board_states: list of board states.
+        move: list of moves, 1:1 with board states.
+        final_score: final score.
+        """
         stuff = zip(board_states, moves)
-        df = ps.DataFrame(index=board_states, columns=tf.Move)
+        df = pd.DataFrame(index=(state_to_string(x) for x in board_states), columns=tf.Move).fillna(0)
+        for state, move in stuff:
+            df.loc[state_to_string(state)].loc[move] = 1
+        self.game_list.append(df)
+        self.game_scores.append(moves)
         pass
+
+    def sort_data(self):
+        """
+        Sorts the list of game scores and game boards by score.
+        """
+
+        # zips the game_list and game_Scores, sorts the result by scores, and then puts them back.
+        self.game_list, self.game_scores = sorted(zip(self.game_list, self.game_scores), key=lambda pair: pair[1])
+
+
+def state_to_string(board_state):
+    """
+    Convert a boardstate to a string, for hashing.
+    """
+    return str(board_state)
--- main/test/test_game_data_storage.py
+++ main/test/test_game_data_storage.py
@@ -1,16 +1,36 @@
 import sys
 sys.path.append('../')
 
-from game_data_storage import DataStorage
+from game_data_storage import DataStorage, state_to_string
+from boardstates_fortesting import *
+from board_transform import Move
 
-boardstate_1 = 
+def test_state_to_string():
+    print("Running test_state_to_string...")
+    assert state_to_string(test_board) == "[[0, 0, 2, 2], [0, 2, 0, 0], [0, 0, 2, 0], [2, 0, 2, 2]]", "Somehow failed tostring"
+    print("Passed test_state_to_string!")
 
 def test_add_game_data(storage):
-    
-    print(storage.game_list)
-    print(storage.game_scores)
-    pass
+    print("Running test_add_game_data...")
 
+    board_list = [test_board, test_board_2]
+    moves_list = [Move.UP, Move.DOWN]
+    score = 20
+
+    storage.add_game_data(board_list, moves_list, score)
+
+    data = storage.game_list[0].loc[state_to_string(test_board)]
+
+    assert data.name == state_to_string(test_board)
+
+    assert data.loc[Move.UP] == 1
+    assert data.loc[Move.LEFT] == 0
+    assert data.loc[Move.DOWN] == 0
+    assert data.loc[Move.RIGHT] == 0
+
+    print("Passed test_add_game_data!")
 
 storage = DataStorage()
+
+test_state_to_string()
 test_add_game_data(storage)
